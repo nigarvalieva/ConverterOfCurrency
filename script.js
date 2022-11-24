0ccc28e52e6b4885889dfac89652205b0d098ae4
nigarvalieva <vvaliyevaa@gmail.com>
2022-11-24 01:57:41 -0800

    fix num in inputs

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ tabOne.forEach((item) => {
             .then(r => r.json()).then(d => {
                 input2.value = input2.value.replace(",", ".").replace("..", ".").replace(".,", ".").replace(",.", ".")
                     .replace(/[^0-9,.]/g, '').split(' ').join('').replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ').replace(/(\.\d+)\s(\d+)/g, '$1$2')
+                input2.value = view(input2.value)
                 if (input1.value.length == '2' && input1.value[0] == '0' && input1.value[1] != '.')
                     input1.value = input1.value.slice(1);
                 if (input1.value.length > 0) {
@@ -53,7 +54,8 @@ tabTwo.forEach((item) => {
         fetch(`https://api.exchangerate.host/latest?base=${valueFirst}&symbols=${valueSecond}`)
             .then(r => r.json()).then(d => {
                 input1.value = input1.value.replace(",", ".").replace("..", ".").replace(".,", ".").replace(",.", ".")
-                    .replace(/[^0-9,.]/g, '').split(' ').join('').replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ').replace(/(\.\d+)\s(\d+)/g, '$1$2')
+                    .replace(/[^0-9,.]/g, '').split(' ').join('')
+                input1.value = view(input1.value)
                 if (input2.value.length == '2' && input2.value[0] == '0' && input2.value[1] != '.')
                     input2.value = input2.value.slice(1);
                 if (input2.value.length > 0) {
@@ -84,7 +86,8 @@ window.addEventListener('click', () => {
             document.querySelector('.currencies-first p').textContent = `1 ${valueFirst} = ${data.rates[valueSecond]} ${valueSecond}`
             input1.addEventListener('input', () => {
                 input1.value = input1.value.replace(",", ".").replace("..", ".").replace(".,", ".").replace(",.", ".")
-                    .replace(/[^0-9,.]/g, '').split(' ').join('').replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ').replace(/(\.\d+)\s(\d+)/g, '$1$2')
+                .replace(/[^0-9,.]/g, '').split(' ').join('').replace(/(\.\d+)\s(\d+)/g, '$1$2')
+                    input1.value = view(input1.value)
                 if (input1.value.length == '2' && input1.value[0] == '0' && input1.value[1] != '.')
                     input1.value = input1.value.slice(1);
                 if (input1.value.length > 0) {
@@ -98,7 +101,8 @@ window.addEventListener('click', () => {
             document.querySelector('.currencies-second p').textContent = `1 ${valueSecond} = ${data.rates[valueFirst]} ${valueFirst}`
             input2.addEventListener('input', () => {
                 input2.value = input2.value.replace(",", ".").replace("..", ".").replace(".,", ".").replace(",.", ".")
-                    .replace(/[^0-9,.]/g, '').split(' ').join('').replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ').replace(/(\.\d+)\s(\d+)/g, '$1$2')
+                    .replace(/[^0-9,.]/g, '').split(' ').join('')
+                input2.value = view(input2.value)
                 if (input2.value.length == '2' && input2.value[0] == '0' && input2.value[1] != '.')
                     input2.value = input2.value.slice(1);
                 if (input2.value.length > 0) {
@@ -108,3 +112,8 @@ window.addEventListener('click', () => {
         })
 })
 
+function view(num){
+    num = num.split('.');
+    num[0] = num[0].replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')
+    return num.join('.');
+}
